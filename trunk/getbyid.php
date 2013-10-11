1286bea56fc87f15902fa9af3683a03323cd6987
regularadam@gmail.com <regularadam@gmail.com@8663118b-b6a3-7db7-00dc-37359b8e79e6>
2013-10-11 17:17:33 +0000

    still trying for ajax fancybox

--- trunk/getbyid.php
+++ trunk/getbyid.php
@@ -1,17 +1,20 @@
 <?php
 $id = $_GET["id"];
 $type = $_GET["type"];
-
+$str = ""
 if($type == "image")
 {
-	echo "<img src=\"http://bluegrit.cs.umbc.edu/~oleg2/instagrams/hurricanesandy/$id.jpg\" />";
+	str .= "<img src=\"http://bluegrit.cs.umbc.edu/~oleg2/instagrams/hurricanesandy/$id.jpg\" />";
 }
 				
 				
-//$(".fancybox-skin")[0].html("<input id=\"poweroutage\" type=\"checkbox\"/>Power Outage");
-//$(".fancybox-skin")[0].append("<input id=\"flooding\" type=\"text\" />");
-//$(".fancybox-skin")[0].append("<input id=\"crime\" type=\"checkbox\"/>Crime");
-//$(".fancybox-skin")[0].append("<input id=\"foodshortage\" type=\"checkbox\"/>Food Shortage");
-//$("#flooding").spinner({max : 20,	min : 0});		
+/*
+str .= "<input id=\"poweroutage\" type=\"checkbox\"/>Power Outage";
+str .= "<input id=\"flooding\" type=\"text\" />";
+str .= "<input id=\"crime\" type=\"checkbox\"/>Crime";
+str .= "<input id=\"foodshortage\" type=\"checkbox\"/>Food Shortage";
+*/
+
+return $str;
 ?>
 
