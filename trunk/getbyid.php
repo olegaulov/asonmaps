60f3ba90d0881730412cc3797d0e647342f348fc
regularadam@gmail.com <regularadam@gmail.com@8663118b-b6a3-7db7-00dc-37359b8e79e6>
2013-11-06 00:07:53 +0000

    corrected table references

--- trunk/getbyid.php
+++ trunk/getbyid.php
@@ -2,6 +2,8 @@
 $id = $_GET["id"];
 $type = $_GET["type"];
 $st = "";
+$db = "";
+$tbl = "";
 
 ini_set("error_log", "/home/adprice1/public_html/error.log");
 ini_set("log_errors", "on");
@@ -9,10 +11,14 @@ ini_set("log_errors", "on");
 if($type == "image")
 {
 	$st .= "<img width=\"80%\" src=\"http://bluegrit.cs.umbc.edu/~oleg2/instagrams/hurricanesandy/$id.jpg\" /><br />";
+	$tbl = "image";
+	$db = "instagramsandy";
 }	
 else 
 {
 	$st .= "<img width=\"80%\" src=\"/asonmaps/tweetpin.png\" /><br />";
+	$tbl = "tweets";
+	$db = "twittersandy";
 }	
 
 $st .= "<input id=\"poweroutage\" type=\"checkbox\" onclick=\"showdiv('powerctl')\"/>Power Outage";
@@ -22,7 +28,7 @@ $st .= "<input id=\"poweroutageoff\" name=\"onoff\" type=\"radio\" />Power Off</
 $st .= "<input id=\"flooding\" type=\"checkbox\" onclick=\"showdiv('feet')\" />Flooding <input type=\"text\" id=\"feet\" style=\"display:none\" /><br />";
 $st .= "<input id=\"crime\" type=\"checkbox\"/>Crime<br />";
 $st .= "<input id=\"foodshortage\" type=\"checkbox\"/>Food Shortage<br />";
-$st .= "<button onclick=\"senddata('$id')\">Save Data</button>";
+$st .= "<button onclick=\"senddata('$db', '$tbl', '$id')\">Save Data</button>";
 
 $st .= "<script type=\"text/javascript\">$('#feet').spinner({max:20, min:1})</script>";
 
