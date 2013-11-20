e4f4f4443c7d34be1c1705e4cc7d30ff5c9c338d
regularadam@gmail.com <regularadam@gmail.com@8663118b-b6a3-7db7-00dc-37359b8e79e6>
2013-11-20 01:04:22 +0000

    made controls slightly more intuitive

--- trunk/getbyid.php
+++ trunk/getbyid.php
@@ -23,19 +23,19 @@ else
 
 $st .= "<input id=\"poweroutage\" type=\"checkbox\" onclick=\"showdiv('powerctl')\"/>Power Outage";
 $st .= "<div id=\"powerctl\" style=\"display:none\">";
-$st .= "<input id=\"poweroutagenull\" name=\"ponoff\" type=\"radio\" />Unknown";
+//$st .= "<input id=\"poweroutagenull\" name=\"ponoff\" type=\"radio\" />Unknown";
 $st .= "<input id=\"poweroutageon\" name=\"ponoff\" type=\"radio\" />Power On";
 $st .= "<input id=\"poweroutageoff\" name=\"ponoff\" type=\"radio\" />Power Off</div><br />";
 //use spinner
 $st .= "<input id=\"flooding\" type=\"checkbox\" onclick=\"showdiv('feet')\" />Flooding <input type=\"text\" id=\"feet\" style=\"display:none\" /><br />";
 $st .= "<input id=\"crime\" type=\"checkbox\" onclick=\"showdiv('crimectl')\"/>Crime<br />";
 $st .= "<div id=\"crimectl\" style=\"display:none\">";
-$st .= "<input id=\"crimenull\" name=\"conoff\" type=\"radio\" />Unknown";
+//$st .= "<input id=\"crimenull\" name=\"conoff\" type=\"radio\" />Unknown";
 $st .= "<input id=\"crimetrue\" name=\"conoff\" type=\"radio\" />True";
 $st .= "<input id=\"crimefalse\" name=\"conoff\" type=\"radio\" />False</div><br />";
 $st .= "<input id=\"foodshortage\" type=\"checkbox\" onclick=\"showdiv('foodctl')\"/>Food Shortage<br />";
 $st .= "<div id=\"foodctl\" style=\"display:none\">";
-$st .= "<input id=\"foodnull\" name=\"fonoff\" type=\"radio\" />Unknown";
+//$st .= "<input id=\"foodnull\" name=\"fonoff\" type=\"radio\" />Unknown";
 $st .= "<input id=\"foodtrue\" name=\"fonoff\" type=\"radio\" />True";
 $st .= "<input id=\"foodfalse\" name=\"fonoff\" type=\"radio\" />False</div><br />";
 $st .= "<button onclick=\"senddata('$db', '$tbl', '$id')\">Save Data</button>";
